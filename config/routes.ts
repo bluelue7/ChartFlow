348e301c8c5fe5112903700cca147da89d0429c1
hzm <3331603092@qq.com>
2026-06-01 17:02:09 +0800

    feat：用户user无法查看日志管理页面

--- config/routes.ts
+++ config/routes.ts
@@ -11,7 +11,7 @@ export default [
   { path: '/add_chart', name: '智能分析（同步）', icon: 'barChartOutlined', component: './AddChart' },
   { path: '/add_chart_async', name: '智能分析（异步）', icon: 'DotChartOutlined', component: './AddChartAsync' },
   { path: '/chart_list', name: '图表列表', icon: 'barChartOutlined', component: './ChartList' },
-  { path: '/log', name: '日志管理', icon: 'fileText', component: './Log' },
+  { path: '/log', name: '日志管理', icon: 'fileText', component: './Log', access: 'canAdmin' },
   { path: '/dataset', name: '数据集', icon: 'database', component: './Dataset' },
   { path: '/feedback_list', name: '反馈列表', icon: 'alertOutlined', component: './FeedbackList' },
   { path: '/prompt_list', name: '提示词', icon: 'messageOutlined', component: './PromptList' },
